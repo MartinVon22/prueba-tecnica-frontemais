356836b2431633267b03c37aed1d5d9fdcf93604
Martin <83525340+MartinVon22@users.noreply.github.com>
2022-09-28 00:47:18 -0300

    Se crea tabla y se agregan condiciones para mostrar/no mostrar la misma dependiendo de la busqueda

--- src/views/MoviesList/MoviesList.tsx
+++ src/views/MoviesList/MoviesList.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { Grid, TextField, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography, Divider } from "@mui/material";
 import { getMovies } from '../../core/services/MovieService';
+import Paper from '@mui/material/Paper';
 
 const MoviesList = () => {
 
-    const [movies, setMovies] = useState();
+    const [movies, setMovies] = useState([]);
     const [searchQuery, setSearchQuery] = useState(""); 
 
     useEffect(() => {
@@ -13,22 +14,53 @@ const MoviesList = () => {
 
     const searchMovies = () => {
         getMovies(searchQuery).then(res => {
-            console.log(res);
+            setMovies(res.data.results)
         });
     }
     return (
-        <Grid container direction="column" alignItems="center" style={{ marginTop: '40px' }}>
-            <Grid item md={12}>
-                <Typography variant="h3" gutterBottom >
-                    Bienvenido a MovieLand
-                </Typography>
-                <Typography variant="subtitle2" gutterBottom>
-                    En este sitio podrás encontrar mucha variedad de Películas entretenidas para ver, busca la que desees y a disfrutar!
-                </Typography>
-                <Divider />
-                <TextField id="standard-basic" label="Título, Fecha de Estreno" variant="standard" style={{ width: '50%' }} onChange={(e) => setSearchQuery(e.target.value)}/>
+        <>
+            <Grid container direction="column" alignItems="center" style={{ marginTop: '40px' }}>
+                <Grid item md={12}>
+                    <Typography variant="h3" gutterBottom >
+                        Bienvenido a MovieLand
+                    </Typography>
+                    <Typography variant="subtitle2" gutterBottom>
+                        En este sitio podrás encontrar mucha variedad de Películas entretenidas para ver, busca la que desees y a disfrutar!
+                    </Typography>
+                    <Divider />
+                    <TextField id="standard-basic" label="Título, Fecha de Estreno" variant="standard" style={{ width: '50%' }} onChange={(e) => setSearchQuery(e.target.value)}/>
+                    {movies.length > 0 && (
+                        <>
+                            <Divider style={{ paddingBottom: '10px' }}/>
+                            <TableContainer component={Paper}>
+                                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                                    <TableHead>
+                                        <TableRow>
+                                        <TableCell></TableCell>
+                                        <TableCell align="center">Título</TableCell>
+                                        <TableCell align="center">Fecha de Estreno</TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {movies.map((movie: any) => (
+                                        <TableRow
+                                            key={movie.original_title}
+                                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                        >
+                                            <TableCell component="th" scope="row"><img src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} width="100" alt=""/>
+                                            </TableCell>
+                                            <TableCell align="center">{movie.original_title}</TableCell>
+                                            <TableCell align="center">{movie.release_date}</TableCell>
+                                        </TableRow>
+                                        ))}
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+                        </>
+                    )}
+                </Grid>
             </Grid>
-        </Grid>
+        </>
     );
 }
 
